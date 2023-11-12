2711de287bf1187933dc38e2f91274552a95476c
natalilazaric <148438642+natalilazaric@users.noreply.github.com>
2023-11-12 13:22:44 +0100

    Update AppConfig.java

--- IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/securityconfig/auth/AppConfig.java
+++ IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/securityconfig/auth/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
 import progi.project.eventovci.user.service.UserService;
 
 
@@ -16,5 +17,9 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
